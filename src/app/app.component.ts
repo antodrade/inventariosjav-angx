1af9551cd5a829f8e47ed584d552117e0fe634b9
prueba2@gmail.com <prueba@gmail.com>
2024-07-23 23:05:16 -0500

    Servicio de Agregar con Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { ProductoListaComponent } from "./producto-lista/producto-lista.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, ProductoListaComponent, RouterModule]
+    imports: [RouterOutlet, ProductoListaComponent, RouterModule, FormsModule]
 })
 export class AppComponent {
   title = 'inventario-app';
